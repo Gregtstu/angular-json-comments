cabafa056b6d05d9be14c1ee0ab2271dd42c98e9
Gregtstu <boltnev.grisha@yandex.ru>
2022-10-09 17:52:17 +0300

    added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {CommentsModule} from "./comments/comments.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {CommentsModule} from "./comments/comments.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommentsModule
+    CommentsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
